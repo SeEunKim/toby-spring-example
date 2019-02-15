ccd49f43640f263e6f01464218202dc2f7adfde8
kimse9450@gmail.com <kimse9450@gmail.com>
2019-02-15 13:36:21 +0900

    example 1-19

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,12 +1,16 @@
 package springbook.user.dao;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        //UserDao dao = (UserDao)context.getBean("userDao"); // 위의 줄과 같은 의미
 
         dao.init();
 
